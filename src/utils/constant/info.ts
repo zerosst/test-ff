132cc8fc874b7586b99e8011c1e59d8b28a63ac7
fish <405343769@qq.com>
2022-05-15 14:59:45 +0800

    修改

--- src/utils/constant/info.ts
+++
@@ -1,6 +0,0 @@
-import { optionsType } from '@/utils/constant/type'
-
-export const GENDER_OPTIONS: optionsType[] = [
-  { value: '0', label: '男', alias: 'man' },
-  { value: '1', label: '女', alias: 'women' },
-]
--- src/utils/formatTime.ts
+++
@@ -1,5 +0,0 @@
-import moment from 'moment'
-
-export function formatTime(time: string, format = 'YYYY-MM-DD') {
-  return moment(time).format(format)
-}
