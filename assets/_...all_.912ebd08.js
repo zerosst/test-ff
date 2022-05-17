bd8cbead5a0bb79c100d6bf8cbee80a4df29a198
fish <405343769@qq.com>
2022-05-17 09:53:16 +0800

    部署测试

--- assets/_...all_.912ebd08.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as _,c}from"./index.266af687.js";const o={};function r(n,t){return _(),c("div",null," Not Found ")}var l=e(o,[["render",r]]);export{l as default};
---
+++ vite.config.ts
@@ -0,0 +1,66 @@
+/// <reference types="vitest" />
+
+import path from 'path'
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import VitePluginCertificate from 'vite-plugin-mkcert';
+import Unocss from 'unocss/vite'
+
+export default defineConfig({
+  base: '/biz/',
+  server: {
+    https: true,
+    // 服务配置
+    hmr: { overlay: false },
+    port: 80, // 类型： number 指定服务器端口;
+    open: true, // 类型： boolean | string在服务器启动时自动在浏览器中打开应用程序；
+    cors: true, // 类型： boolean | CorsOptions 为开发服务器配置 CORS。默认启用并允许任何源
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
+  },
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    Vue({
+      reactivityTransform: true,
+    }),
+    VitePluginCertificate({
+      source: 'coding',
+    }),
+    // https://github.com/hannoeru/vite-plugin-pages
+    Pages(),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        'vue/macros',
+        'vue-router',
+        '@vueuse/core',
+      ],
+      dts: true,
+    }),
+
+    // https://github.com/antfu/vite-plugin-components
+    Components({
+      dts: true,
+    }),
+
+    // https://github.com/antfu/unocss
+    // see unocss.config.ts for config
+    Unocss(),
+  ],
+
+  // https://github.com/vitest-dev/vitest
+  test: {
+    environment: 'jsdom',
+  },
+})
